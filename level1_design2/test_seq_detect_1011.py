c9d0bd290d135ce739365b2fc039db44c9449b36
sameeryada <sameer.yadav@iiitdmj.ac.in>
2022-07-26 11:20:56 +0000

    modified:   level1_design2/test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -22,8 +22,11 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)  
     dut.reset.value = 0
     await FallingEdge(dut.clk)
-   
+
+    dut._log.info(f'Input bit: {dut.inp_bit.value}')
+
     cocotb.log.info('#### CTB: Develop your test here! ######')
+    
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
@@ -34,5 +37,18 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
+
+    #dut.inp_bit.value = 1
+    #await FallingEdge(dut.clk)
+    #dut.inp_bit.value = 0
+    #await FallingEdge(dut.clk)
+    #dut.inp_bit.value = 1
+    #await FallingEdge(dut.clk)
+    #dut.inp_bit.value = 0
+    #await FallingEdge(dut.clk)
+    #dut.inp_bit.value = 1
+    #await FallingEdge(dut.clk)
+    #dut.inp_bit.value = 1
+    #await FallingEdge(dut.clk)
 
     assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 11011. Model Output: {dut.seq_seen.value} Expected Ouput: 1'
