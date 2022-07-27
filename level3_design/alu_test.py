56fe64fe9cbffd1100ffe6fce862d193a6dab6c5
sameeryada <sameer.yadav@iiitdmj.ac.in>
2022-07-27 09:02:48 +0000

    modified:   level3_design/Makefile 	modified:   level3_design/alu_test.py

--- level3_design/alu_test.py
+++ level3_design/alu_test.py
@@ -21,4 +21,6 @@ async def alu_test(dut):
 
     await Timer(2, units='ns')
 
-    assert dut.ALU_Result.value == A1+B1, f"Subtractor result is incorrect: {dut.X.value} != 10"
+    #assert dut.ALU_Result.value == A1+B1, f"Subtractor result is incorrect: {dut.X.value} != 10"
+    #assert dut.ALU_Out.value == A1+B1, "ALU Subtractor result is incorrect: {A_inp30} != {OUT}, expected value={EXP}".format(A_inp30=int(dut.inp30.value), B_sel=int(dut.sel.value), OUT=int(dut.out.value), EXP=A_inp30)
+    assert dut.ALU_Out.value == A1-B1, "ALU Subtractor result is incorrect: {A1} - {B1} != {ALU_Out}, expected value={EXP}".format(A1=int(dut.A.value), B1=int(dut.B.value), ALU_Out=int(dut.ALU_Out.value), EXP=A1-B1)
