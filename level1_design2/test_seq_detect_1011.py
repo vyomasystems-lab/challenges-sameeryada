a4e6671cf7c2be3346c5282917aca7a9204f5c91
sameeryada <sameer.yadav@iiitdmj.ac.in>
2022-07-27 10:09:12 +0000

    modified:   level1_design2/test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -27,7 +27,20 @@ async def test_seq_bug1(dut):
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
     
+    #dut.inp_bit.value = 1
+    #await FallingEdge(dut.clk)
+    #dut.inp_bit.value = 1
+    #await FallingEdge(dut.clk)
+    #dut.inp_bit.value = 0
+    #await FallingEdge(dut.clk)
+    #dut.inp_bit.value = 1
+    #await FallingEdge(dut.clk)
+    #dut.inp_bit.value = 1
+    #await FallingEdge(dut.clk)
+
     dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
@@ -38,17 +51,4 @@ async def test_seq_bug1(dut):
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
 
-    #dut.inp_bit.value = 1
-    #await FallingEdge(dut.clk)
-    #dut.inp_bit.value = 0
-    #await FallingEdge(dut.clk)
-    #dut.inp_bit.value = 1
-    #await FallingEdge(dut.clk)
-    #dut.inp_bit.value = 0
-    #await FallingEdge(dut.clk)
-    #dut.inp_bit.value = 1
-    #await FallingEdge(dut.clk)
-    #dut.inp_bit.value = 1
-    #await FallingEdge(dut.clk)
-
-    assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 11011. Model Output: {dut.seq_seen.value} Expected Ouput: 1'
+    assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 101011. Model Output: {dut.seq_seen.value} Expected Ouput: 1'
